a7c2c682a40c8731f2f6e868efb6b8aecdfafaa7
Martin Fenner <martin@front-matter.io>
2023-01-23 15:32:45 +0100

    fixed build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,21 +9,16 @@ import { Inter } from '@next/font/google'
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['latin'] })
 
-export default function App({
-  Component,
-  pageProps,
-}: AppProps<{
-    initialSession: Session
-  }>) {
+export default function App({ Component, pageProps }) {
   // Create a new supabase browser client on every first render.
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
-  
+
   return (
-    <PlausibleProvider domain='rogue-scholar.org'>
+    <PlausibleProvider domain="rogue-scholar.org">
       <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
         <main className={inter.className}>
           <Component {...pageProps} />
         </main>
