26a5cd9f9b7b5b63a2222d6c8c52e02f2efd154d
Martin Fenner <martin@front-matter.io>
2023-01-26 17:42:13 +0100

    adding docs configuration

---
+++ apps/docs/README.md
@@ -0,0 +1,7 @@
+# The Rogue Scholar Documentation
+
+This is a documentation repository for the Rogue Scholar project. It is built using [Nextra](https://nextra.site).
+
+## License
+
+This project is licensed under the MIT License.
---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>The Rogue Scholar Documentation</span>,
+  project: {
+    link: 'https://rogue-scholar.org',
+  },
+  chat: {
+    link: 'https://discord.gg/wJAxWjWU',
+  },
+  docsRepositoryBase: 'https://github.com/front-matter/docs/tree/main',
+  footer: {
+    text: 'The Rogue Scholar Documentation',
+  },
+}
+
+export default config
