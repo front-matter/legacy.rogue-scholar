13700ee52e8aa2bb12037317896a380609b4e1f5
Martin Fenner <martin@front-matter.io>
2023-01-27 14:56:37 +0100

    customize header

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
+import Link from 'next/link'
+import Icon from './public/icon.png'
 
 const config: DocsThemeConfig = {
-  logo: <span>The Rogue Scholar Documentation</span>,
-  project: {
-    link: 'https://rogue-scholar.org',
-  },
+  logo: (
+    <div className="nx-flex nx-justify-center nx-text-2xl nx-font-bold">
+      <Image src={Icon} alt="Icon" width={18} className="mr-3" />
+      <span style={{ marginLeft: '.4em', marginRight: '.2em', fontWeight: 700, color: 'rgb(37, 99, 235)' }}>The Rogue Scholar</span>Documentation
+    </div>
+  ),
   chat: {
     link: 'https://discord.gg/wJAxWjWU',
   },
@@ -13,6 +18,16 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'The Rogue Scholar Documentation',
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="The Rogue Scholar Documentation" />
+      <meta
+        property="og:description"
+        content="Documentation for the Rogue Scholar website."
+      />
+    </>
+  ),
 }
 
 export default config
