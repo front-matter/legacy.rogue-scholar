da47ad6892422b98577d8cfee211aebf6cafce02
Martin Fenner <martin@front-matter.io>
2023-04-06 14:07:00 +0200

    configure i18next

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config')
+
 const nextConfig = {
+  I18n,
   reactStrictMode: true,
   experimental: {
     scrollRestoration: true,
