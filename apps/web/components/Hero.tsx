e41f01dfaedabeeaadfe15a8a88e1daa3b70fe81
Martin Fenner <martin@front-matter.io>
2023-04-16 10:37:55 +0200

    Update Hero.tsx

--- apps/web/components/Hero.tsx
+++ apps/web/components/Hero.tsx
@@ -38,7 +38,7 @@ export const Hero: React.FunctionComponent<Props> = ({ blogs }) => {
         </h2>
         <div className="mx-auto mt-6 grid max-w-lg grid-cols-6 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
           {blogsWithIimages.map((blog) => (
-            <Link key={blog.id} href={'/blogs/' + blog.id}>
+            <Link key={blog.id} href={'/' + blog.id}>
               <img
                 className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
                 src={blog.favicon}
