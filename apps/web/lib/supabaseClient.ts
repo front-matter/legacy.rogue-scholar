704640260f9ff3c5a284936ad4be9fa91d27be64
Martin Fenner <martin@front-matter.io>
2023-06-10 11:08:33 +0200

    adding new api endpoints

--- apps/web/lib/supabaseClient.ts
+++ apps/web/lib/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string)
 
-export const blogsSelect = 'id, title, description, language, favicon, feed_url, feed_format, home_page_url, indexed_at, license, generator, category'
+export const blogsSelect = 'id, title, description, language, favicon, feed_url, feed_format, home_page_url, indexed_at, modified_at, license, generator, category'
 
 export const postsSelect = 'id, uuid, url, title, summary, date_published, date_modified, authors, image, content_html, tags, language, blog_id, blog: blogs!inner(*)'
 
