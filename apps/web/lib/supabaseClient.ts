67e8193f7b8c423440e3802c46703cd43b221850
Martin Fenner <martin@front-matter.io>
2023-06-10 10:25:43 +0200

    Update supabaseClient.ts

--- apps/web/lib/supabaseClient.ts
+++ apps/web/lib/supabaseClient.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string)
 
-export const blogsSelect = 'id, title, description, language, icon, favicon, feed_url, feed_format, home_page_url, indexed_at, license, generator, category'
+export const blogsSelect = 'id, title, description, language, favicon, feed_url, feed_format, home_page_url, indexed_at, license, generator, category'
 
 export const postsSelect = 'id, uuid, url, title, summary, date_published, date_modified, authors, image, content_html, tags, language, blog_id, blog: blogs!inner(*)'
 
-export const blogWithPostsSelect = 'id, title, description, language, icon, favicon, feed_url, feed_format, home_page_url, indexed_at, license, generator, category, items: posts (id, uuid, url, title, summary, date_published, date_modified, authors, image, content_html, tags, language)'
+export const blogWithPostsSelect = 'id, title, description, language, favicon, feed_url, feed_format, home_page_url, indexed_at, license, generator, category, items: posts (id, uuid, url, title, summary, date_published, date_modified, authors, image, content_html, tags, language)'
