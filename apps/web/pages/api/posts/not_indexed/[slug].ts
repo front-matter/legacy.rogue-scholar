a5b7af6033397334cd7b3e1f9c164b76332628e4
Martin Fenner <martin@front-matter.io>
2023-06-17 18:46:26 +0200

    only show posts to update that have a doi

--- apps/web/pages/api/posts/not_indexed/[slug].ts
+++ apps/web/pages/api/posts/not_indexed/[slug].ts
@@ -1,7 +1,7 @@
 import { supabase, postsSelect } from '@/lib/supabaseClient';
 
 export default async function handler(req, res) {
-  let { data: posts, error } = await supabase.from('posts').select(postsSelect).lt('date_indexed', req.query.slug).limit(15) 
+  let { data: posts, error } = await supabase.from('posts').select(postsSelect).not('blogs.prefix','is', 'null').like('id', '%doi_org%').lt('date_indexed', req.query.slug).limit(15) 
 
   if (error) {
     console.log(error);
