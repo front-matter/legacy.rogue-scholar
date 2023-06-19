c3ee1a1d79d7dd6894c0fe66ad53287ed1c57b1a
Martin Fenner <martin@front-matter.io>
2023-06-19 13:40:31 +0200

    remove obsolete code

--- apps/web/pages/api/posts.ts
+++ apps/web/pages/api/posts.ts
@@ -22,28 +22,6 @@ export async function getPosts(blogSlug: string) {
   }
 };
 
-export async function upsertPost(post: PostType, blog_id: string) {
-  const { data, error } = await supabaseAdmin.from('posts').upsert({
-    id: post.id,
-    title: post.title,
-    summary: post.summary,
-    tags: post.tags,
-    authors: post.authors,
-    image: post.image,
-    date_published: post.date_published,
-    date_modified: post.date_modified,
-    date_indexed: new Date().toISOString(),
-    content_html: post.content_html,
-    blog_id: blog_id,
-  });
-
-  if (error) {
-    throw error;
-  }
-
-  return data;
-}
-
 export default async function handler(_, res) {
   let { data: posts, error } = await supabase.from('posts').select(postsSelect).range(0, 24).order('date_published', { ascending: false })
 
