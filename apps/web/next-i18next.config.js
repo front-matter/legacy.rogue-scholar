72dcec5883de6da31a8220e662cc73664990c024
Martin Fenner <martin@front-matter.io>
2023-07-07 09:41:01 +0200

    load translation files from fs

--- apps/web/next-i18next.config.js
+++ apps/web/next-i18next.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['de', 'en'],
+    defaultLocale: "en",
+    locales: ["de", "en"],
   },
+  localePath:
+    typeof window === "undefined"
+      ? require("path").resolve("./public/locales")
+      : "/public/locales",
   react: { useSuspense: false },
-};
+}
