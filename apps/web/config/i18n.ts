54bdd267a46ee46cfbca37207b103cd42772a5c3
Martin Fenner <martin@front-matter.io>
2023-07-22 08:36:10 +0200

    i18n support for spanish

--- apps/web/config/i18n.ts
+++ apps/web/config/i18n.ts
@@ -1,17 +1,19 @@
-import { i18n } from '@/next-i18next.config';
+import { i18n } from "@/next-i18next.config"
 
 export const localeNames = {
-  de: 'Deutsch',
-  en: 'English',
-};
+  de: "Deutsch",
+  en: "English",
+  es: "Español",
+}
 
-export type LocaleCode = keyof typeof localeNames;
+export type LocaleCode = keyof typeof localeNames
 
-export const defaultLocale = (i18n?.defaultLocale || 'en') as LocaleCode;
+export const defaultLocale = (i18n?.defaultLocale || "en") as LocaleCode
 
 export const localeCurrencies = {
-  de: 'EUR',
-  en: 'USD',
-};
+  de: "EUR",
+  en: "USD",
+  es: "EUR",
+}
 
-export const defaultCurrency = localeCurrencies[defaultLocale];
+export const defaultCurrency = localeCurrencies[defaultLocale]
