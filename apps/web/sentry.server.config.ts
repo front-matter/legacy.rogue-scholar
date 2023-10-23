956464f55cd8c46b8c09b57080c90a5806813a20
Martin Fenner <martin@front-matter.io>
2023-10-23 10:06:20 +0200

    adding sentry support

---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://93bb2e6d210ac5e3277e1870a548c273@o4505997800701952.ingest.sentry.io/4506098541264896",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
