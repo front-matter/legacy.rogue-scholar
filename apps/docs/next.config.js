8217f8a586246a2fb2c4071201fbaa049cc655b2
Martin Fenner <martin@front-matter.io>
2023-11-26 19:27:18 +0100

    Update next.config.js

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,11 +1,14 @@
+const { withPlausibleProxy } = require('next-plausible')
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra({
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en'
-  }
-})
+module.exports = withPlausibleProxy()(
+  withNextra({
+    i18n: {
+      locales: ['en'],
+      defaultLocale: 'en',
+    },
+  })
+)
