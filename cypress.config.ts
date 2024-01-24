69c7ff9320a57392d733101c5ff444352b8e758f
Martin Fenner <martin@front-matter.io>
2024-01-24 22:45:50 +0100

    tweak cypress github action

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   projectId: "hp59u9",
+  video: false,
+  retries: 3,
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: "http://localhost:3000",
+    specPattern: 'cypress/e2e/**/*.cy.js',
   },
 })
