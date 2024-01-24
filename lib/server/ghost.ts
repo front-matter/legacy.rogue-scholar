e878a7c3d63f8ff7020cb6640e4c7a9f049fbc82
Martin Fenner <martin@front-matter.io>
2024-01-24 15:38:27 +0100

    remove obsolete code

--- lib/server/ghost.ts
+++
@@ -1,9 +0,0 @@
-import GhostContentAPI from "@tryghost/content-api"
-
-export async function ghostApi(url: string, slug: string) {
-  return new GhostContentAPI({
-    url,
-    key: process.env[`NEXT_PUBLIC_${slug.toUpperCase()}_GHOST_API_KEY`],
-    version: "v5.0",
-  })
-}
