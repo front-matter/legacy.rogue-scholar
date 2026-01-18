d018e80c82c7b8973279aaa42ba9d975b244037b
Martin Fenner <martin@front-matter.io>
2026-01-18 23:23:27 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const { i18n } = require("./next-i18next.config")
 
 const nextConfig = {
+  output: 'standalone',
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
